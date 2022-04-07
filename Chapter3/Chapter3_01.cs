559c58a1b160be2d811dced3d9af3358dfea3fee
Peter Murray <peter@DESKTOP-SLBRQ8I>
2022-04-07 06:50:39 +0100

    Section 3.1 Complete

---
+++ Chapter3/Chapter3_01.cs
@@ -0,0 +1,82 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Chapter3
+{
+    public class Chapter3_01 : IChapter
+    {
+        // API to use for example
+        public async Task Run()
+        {
+            await foreach (var val in GetValuesAsync())
+            {
+                Console.WriteLine($"Value {val}");
+            }
+
+            // This will query the API for ten Pokemon at a time then
+            // print each one before launching the next query to get
+            // the next ten and so on. This asynchronous stream allows
+            // the processing to happen without having to fetch and
+            // populate the full list of pokemon first
+            Console.WriteLine($"Printing Pokemon names in blocks of 10");
+            await foreach (var val in GetValuesAsync(new HttpClient()))
+            {
+                Console.WriteLine($"Value {val}");
+            }
+        }
+        async IAsyncEnumerable<int> GetValuesAsync()
+        {
+            await Task.Delay(1000); // some asynchronous work
+            yield return 10;
+            await Task.Delay(1000); // more asynchronous work
+            yield return 13;
+        }
+
+        async IAsyncEnumerable<string> GetValuesAsync(HttpClient client)
+        {
+            int offset = 0;
+            const int limit = 10;
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            while (true)
+            {
+                // Get the current page of results and parse them
+                string result = await client.GetStringAsync($"https://pokeapi.co/api/v2/pokemon?limit={limit}&offset={offset}");
+                var pokemonResults = JsonSerializer.Deserialize<PokemonResults>(result, options);
+                string[] valuesOnThisPage = pokemonResults.Results.Select(x => x.Name).ToArray();
+
+                // Produce the results for this page
+                foreach (string value in valuesOnThisPage)
+                    yield return value;
+
+                // If this is the last page, we're done
+                if (valuesOnThisPage.Length != limit)
+                    break;
+
+                // Otherwise, proceed to the next page
+                offset += limit;
+            }
+        }
+        public class PokemonResults
+        {
+            public int Count { get; set; }
+            public string Next { get; set; }
+            public string Previous { get; set; }
+            public IReadOnlyCollection<Pokemon> Results { get; set; }
+
+        }
+        public class Pokemon
+        {
+            public string Name { get; set; }
+            public string Url { get; set; }
+        }
+    }
+}
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Chapter2;
+using Chapter3;
 using System.Threading.Tasks;
 
 namespace TestApp
@@ -7,39 +8,45 @@ namespace TestApp
     {
         static async Task Main(string[] args)
         {
-            //var chapter2_1 = new Chapter2_1();
+            #region Chapter2
+            //Chapter2_01 chapter2_1 = new();
             //await chapter2_1.Run();
 
-            //var chapter2_2 = new Chapter2_2();
+            //Chapter2_02 chapter2_2 = new();
             //await chapter2_2.Run();
 
-            //var chapter2_3 = new Chapter2_3();
+            //Chapter2_03 chapter2_3 = new();
             //await chapter2_3.Run();
 
-            //var chapter2_4 = new Chapter2_4();
+            //Chapter2_04 chapter2_4 = new();
             //await chapter2_4.Run();
 
-            //Chapter2_5 chapter2_5 = new();
+            //Chapter2_05 chapter2_5 = new();
             //await chapter2_5.Run();
 
-            //Chapter2_6 chapter2_6 = new();
+            //Chapter2_06 chapter2_6 = new();
             //await chapter2_6.Run();
 
-            //Chapter2_7 chapter2_7 = new();
+            //Chapter2_07 chapter2_7 = new();
             //await chapter2_7.Run();
 
-            //Chapter2_8 chapter2_8 = new();
+            //Chapter2_08 chapter2_8 = new();
             //await chapter2_8.Run();
 
-            //Chapter2_9 chapter2_9 = new();
+            //Chapter2_09 chapter2_9 = new();
             //await chapter2_9.Run();
 
             //Chapter2_10 chapter2_10 = new();
             //await chapter2_10.Run();
 
-            Chapter2_11 chapter2_11 = new();
-            await chapter2_11.Run();
+            //Chapter2_11 chapter2_11 = new();
+            //await chapter2_11.Run();
+            #endregion
 
+            #region Chapter3
+            Chapter3_01 chapter3 = new();
+            await chapter3.Run();
+            #endregion
         }
     }
 }
