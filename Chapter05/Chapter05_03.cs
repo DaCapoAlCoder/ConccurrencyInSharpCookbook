a28db6c60484d2f79f20db1fa5f13e8c2d43ad7d
Peter Murray <peter@DESKTOP-SLBRQ8I>
2022-06-02 12:43:16 +0100

    Section5.3 Complete

--- Chapter05/Chapter05_03.cs
+++ Chapter05/Chapter05_03.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 
@@ -12,6 +13,11 @@ namespace Chapter05
     {
         public async Task Run()
         {
+            // unlinking can be used to change filter dynamically
+            // It can also be used to pause a data flow mesh
+            // There is a race condition in terms of data processing and the disposal/unlinking
+            // There won't be any duplication of data or data loss
+            // Either that data will flow through the block or it won't
             await Test();
         }
 
@@ -26,15 +32,25 @@ namespace Chapter05
             multiplyBlock.Post(2);
             multiplyBlock.Post(3);
 
-            multiplyBlock.Complete();
-            await multiplyBlock.Completion;
-            var valuesBeforeUnlink = subtractBlock.ReceiveAllAsync();
-            await foreach(int value in valuesBeforeUnlink )
-            {
-                Console.WriteLine($"Value before unlinking {value}");
-            }
+            using CancellationTokenSource cts = new();
+            var valuesBeforeUnlink = subtractBlock.ReceiveAllAsync(cts.Token);
+            int i = 0;
 
-            
+            try
+            {
+                await foreach (int value in valuesBeforeUnlink)
+                {
+                    if (++i == 2)
+                    {
+                        cts.Cancel();
+                    }
+                    Console.WriteLine($"Value before unlinking {value}");
+                }
+
+            }
+            catch (TaskCanceledException)
+            {
+            }
 
             // Unlink the blocks.
             // The data posted above may or may not have already gone through the link.
@@ -42,8 +58,10 @@ namespace Chapter05
             link.Dispose();
 
             multiplyBlock.Post(4);
+            multiplyBlock.Complete();
             int valueAfterUnlink = await multiplyBlock.ReceiveAsync(); 
             Console.WriteLine($"The value after unlinking {valueAfterUnlink}");
+            await multiplyBlock.Completion;
         }
     }
 }
